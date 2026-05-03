ec3230cc4cc4ade50dbc31448e59cdd2165243da
dev <234845549+adrazv@users.noreply.github.com>
2026-05-03 19:27:18 -0300

    test16

--- index.js
+++ index.js
@@ -58,16 +58,41 @@ app.post('/api/shorturl', (req, res) => {
   }
 });
 
+
 app.get('/api/shorturl/:short_url', (req, res) => {
   const id = parseInt(req.params.short_url);
   const destination = urlDatabase[id];
 
   if (destination) {
-    res.redirect(destination);
+    res.status(301).location(destination).send();
   } else {
     res.json({ error: "No short URL found" });
   }
 });
+/*
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const id = parseInt(req.params.short_url);
+  const destination = urlDatabase[id];
+
+  if (destination) {
+    res.writeHead(301, { 'Location': destination });
+    res.end();
+  } else {
+    res.json({ error: "No short URL found" });
+  }
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const id = parseInt(req.params.short_url);
+  const destination = urlDatabase[id];
+
+  if (destination) {
+    res.redirect(301, destination);
+  } else {
+    res.json({ error: "No short URL found" });
+  }
+});
+*/
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
